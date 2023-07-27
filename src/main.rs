fe3b2109654a7036aba1f4cf9d2bbe07aea50b5d
SkuldNorniern <skuldnorniern@gmail.com>
2023-07-27 11:27:43 +0900

    feat: init project with cli

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use clap::{Arg, ArgAction, Command};
+
+use std::process::exit;
+
+fn cli() -> Command {
+    Command::new("pacify")
+        .version("0.1.0")
+        .author("Skuld Norniern. <skuldnorniern@gmail.com>")
+        .about("Yet another Python build/package manager")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initialize a new project")
+                .arg(
+                    Arg::new("path")
+                        .help("Project name")
+                        .short('n')
+                        .long("name")
+                        .value_name("NAME")
+                        .action(ArgAction::Set)
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            Command::new("build")
+                .about("Build a project")
+        ) 
+        .subcommand(
+            Command::new("new")
+                .about("Create a new project")
+        )
+        .subcommand(
+            Command::new("run")
+        )
+        .subcommand(
+            Command::new("test")
+        )
+        .subcommand(
+            Command::new("clean")
+        )
+        .subcommand(
+            Command::new("add")
+                .about("Add a dependency")
+                .arg(
+                    Arg::new("package name")
+                        .help("Package name")
+                        .action(ArgAction::Set)
+                        .num_args(1..)
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("version")
+                        .help("Version")
+                        .short('v')
+                        .long("version")
+                        .action(ArgAction::Set)
+                )
+        )
+        .subcommand(
+            Command::new("update")
+                .about("Update a package")
+                .arg(
+                    Arg::new("package name")
+                        .help("Package name")
+                        .action(ArgAction::Set)
+                        .num_args(1..)
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            Command::new("remove")
+        )
+}
+
+fn main() {
+
+    let args = cli().get_matches();
+
+
+    //match args.subcommand() {
+        
+    //}
+}
